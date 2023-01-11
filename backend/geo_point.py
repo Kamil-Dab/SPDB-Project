702e1fbce14370ffab7469e4df201efebc5c6713
Kamil Dabrowski <kamild973@gmail.com>
2023-01-12 00:21:49 +0100

    fixes in GeoPoint and GeoRoad

--- backend/geo_point.py
+++ backend/geo_point.py
@@ -1,7 +1,7 @@
 from db.engine import get_session
 from models import Poi
 from shapely.geometry import Point
-from geoalchemy2.shape import from_shape, to_shape
+from geoalchemy2.shape import from_shape
 from sqlalchemy import func
 from pyproj import Proj, transform
 
@@ -9,18 +9,18 @@ from pyproj import Proj, transform
 class GeoPoint:
     def __init__(self):
         self.session = get_session()
+        self.inProj = Proj('epsg:3857')
+        self.outProj = Proj('epsg:4326')
     
-    def nearest_points(self, latitude, longitude, n, distance=5000):
+    def nearest_points(self, longitude, latitude, n, distance=5000):
         coord = Point(longitude, latitude)
         filter = from_shape(coord, srid=4326).ST_Transform(3857).ST_Buffer(distance)
         buffor_points = self.session.query(Poi).filter(Poi.geom.ST_Intersects(filter)).subquery()
         points = self.session.query(buffor_points).order_by(func.ST_Distance(buffor_points.c.geom, from_shape(coord, srid=3857))).limit(n).all()
-        inProj = Proj('epsg:3857')
-        outProj = Proj('epsg:4326')
-        return [[transform(inProj, outProj, to_shape(k.geom).x, to_shape(k.geom).y), k.name] for k in points]
+        return [[transform(self.inProj, self.outProj, *k.get_coords()), k.name] for k in points]
 
     def nearest_points_by_name(self, name, n):
         points = self.session.query(Poi).order_by(func.difference(Poi.name, name).desc(),func.levenshtein(Poi.name, name)).limit(n).all()
         inProj = Proj('epsg:3857')
         outProj = Proj('epsg:4326')
-        return [[transform(inProj, outProj, to_shape(k.geom).x, to_shape(k.geom).y), k.name] for k in points]
+        return [[transform(self.inProj, self.outProj, *k.get_coords()), k.name] for k in points]
--- backend/geo_road.py
+++ backend/geo_road.py
@@ -7,7 +7,7 @@ from sqlalchemy import func
 
 
 class GeoRoad:
-    def __init__(self, start_latitude, start_longitude, end_latitude, end_longitude):
+    def __init__(self, start_longitude, start_latitude, end_longitude, end_latitude):
         self.start_coord = Point(start_longitude, start_latitude)
         self.end_coord = Point(end_longitude, end_latitude)
         self.session = get_session()
@@ -16,16 +16,17 @@ class GeoRoad:
         return self.session.query(Road).order_by(func.ST_Distance(func.ST_StartPoint(Road.geom), from_shape(self.start_coord, srid=4326), True)).limit(1).one()
 
     def get_nearest_end_point(self):
-        return self.session.query(Road).order_by(func.ST_Distance(func.ST_EndPoint(Road.geom), from_shape(self.end_coord, srid=4326), True)).limit(1).one()
+        return self.session.query(Road).order_by(func.ST_Distance(func.ST_StartPoint(Road.geom), from_shape(self.end_coord, srid=4326), True)).limit(1).one()
 
     def shortest_path(self):
-        QUERY_SHORTEST_PATH=  f"""SELECT seq, node, edge, cost as cost, agg_cost, geom, ST_AsText(geom)
+        QUERY_SHORTEST_PATH=  f"""SELECT ST_AsText(geom)
                 FROM pgr_dijkstra(
                    'SELECT id, source, target, st_length(geom, true) as cost FROM roads',
                    {self.get_nearest_start_point().source},
-                   {self.get_nearest_end_point().target}
+                   {self.get_nearest_end_point().source},
+                   false
                 ) as pt
-                JOIN roads rd ON pt.edge = rd.id;"""
+                JOIN roads rd ON pt.edge = rd.id ;"""
         result = self.session.bind.execute(QUERY_SHORTEST_PATH).all()
         convert_result = [[*row[:-1], list(loads(row[-1]).coords)] for row in result]
         return convert_result
@@ -43,12 +44,16 @@ class GeoRoad:
                 FROM pgr_dijkstra(
                     'SELECT id, source, target, st_length(geom, true) as cost FROM roads',
                     {self.get_nearest_start_point().source},
-                    {self.get_nearest_end_point().target}
+                    {self.get_nearest_end_point().target},
+                    false
                 ) as pt
                 JOIN roads rd ON pt.edge = rd.id
                 WHERE pt.agg_cost <= {distance}
                 ORDER BY agg_cost DESC LIMIT 1
             ) as foo;
         """
-        point = self.session.bind.execute(QUERY_POINT_IN_PATH).all()[0][:]
-        return point
+        point = self.session.bind.execute(QUERY_POINT_IN_PATH).all()
+        if not point:
+            return []
+
+        return point[0][:]
