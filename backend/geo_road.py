891690a67dc799cc41dae4ba0121aff5ca6026bc
Kamil Dabrowski <kamild973@gmail.com>
2023-01-13 15:43:25 +0100

    fixed shortest path

--- backend/geo_road.py
+++ backend/geo_road.py
@@ -26,32 +26,19 @@ class GeoRoad:
         return self.session.query(Road).order_by(func.ST_Distance(func.ST_StartPoint(Road.geom), from_shape(self.end_coord, srid=4326), True)).limit(1).one()
 
     def shortest_path(self):
-        QUERY_SHORTEST_PATH=  f"""
-            DROP TABLE IF EXISTS {self.table_name};
-            CREATE TABLE {self.table_name} AS
-            SELECT agg_cost, cost as cost,
-                CASE
-                    WHEN pt.node = rd.source THEN geom
-                    ELSE ST_Reverse(geom)
-                END AS geom,
-                CASE
-                    WHEN pt.node = rd.source THEN ST_AsText(geom)
-                    ELSE ST_AsText(ST_Reverse(geom))
-                END AS geom_text
-            FROM pgr_astar('
-                SELECT id, source, target, st_length(geom, true) as cost,
-                    st_x(st_startpoint(geom)) as x1,
-                    st_y(st_startpoint(geom)) as y1,
-                    st_x(st_endpoint(geom)) as x2,
-                    st_y(st_endpoint(geom)) as y2   
-                FROM roads',
-                    {self.get_nearest_start_point().source},
-                    {self.get_nearest_end_point().source},
-                    false
+        QUERY_SHORTEST_PATH=  f"""SELECT agg_cost,
+        CASE
+            WHEN pt.node = rd.source THEN ST_AsText(geom)
+            ELSE ST_AsText(ST_Reverse(geom))
+        END AS geom
+                FROM pgr_dijkstra(
+                   'SELECT id, source, target, st_length(geom, true) as cost FROM roads',
+                   {self.get_nearest_start_point().source},
+                   {self.get_nearest_end_point().source},
+                   false
                 ) as pt
-            JOIN roads rd ON pt.edge = rd.id 
-            ORDER BY seq;
-            SELECT * FROM {self.table_name};"""
+                JOIN roads rd ON pt.edge = rd.id
+                ORDER BY seq;"""
         result = self.session.bind.execute(QUERY_SHORTEST_PATH).all()
         all_points = []
         self.path = []
